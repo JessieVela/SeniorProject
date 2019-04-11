bf510888af10be78a5247a3ce8f84715878e7fcd
DylanPatel <patelo.dylan@gmail.com>
2019-04-11 09:49:11 -0700

    Minor changes to the popup dialog, etc.

---
+++ app/src/main/java/com/scrumptious6/xtract/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.scrumptious6.xtract;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity{
+    private final int SPLASH_DURATION = 5000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
