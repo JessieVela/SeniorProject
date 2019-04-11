c8dcdce6241ba41e8046d3d8d8789d59529f6ee8
Ali <alio.sameer@gamil.com>
2019-04-11 09:50:51 -0700

    EmailActivity integrated with ExportActivity

--- app/src/main/java/com/scrumptious6/xtract/email.java
+++ app/src/main/java/com/scrumptious6/xtract/email.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class EmailActivity extends AppCompatActivity {
+public class email extends AppCompatActivity {
     private EditText mEditTextTo;
     private EditText mEditTextSubject;
     private EditText mEditTextMessage;
